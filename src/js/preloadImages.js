e6b3367ebf6aa47edf14a48dfa3ec54b8f2f90c8
Aliaksei Siniauski <92273438+aliaksei-siniauski@users.noreply.github.com>
2022-09-11 12:50:53 +0200

    change images path

--- src/js/preloadImages.js
+++ src/js/preloadImages.js
@@ -6,7 +6,7 @@ const preloadImages = () => {
   seasons.forEach((element) => {
     for (let i = 1; i <= 6; i++) {
       const img = new Image();
-      img.src = `/src/images/portfolio/${element}/${i}.jpg`;
+      img.src = `src/images/portfolio/${element}/${i}.jpg`;
     }
   });
 };
